d18fa514e07810f8c8235fadbcad1ff60eda2d93
Jung Choi <jung.choi@crowdstrike.com>
2025-05-02 10:52:32 -0400

    add method to get Falcon image registry URI for testing

--- falcon/containers.go
+++ falcon/containers.go
@@ -5,6 +5,7 @@ import (
 )
 
 type SensorType string
+type CSRegistryEnv string
 
 const (
 	SidecarSensor  SensorType = "falcon-container"
@@ -15,6 +16,11 @@ const (
 	FCSCli         SensorType = "fcs"
 	SHRAController SensorType = "falcon-jobcontroller"
 	SHRAExecutor   SensorType = "falcon-registryassessmentexecutor"
+)
+
+const (
+	Stage   CSRegistryEnv = "stage"
+	Release CSRegistryEnv = "release"
 )
 
 // FalconContainerUploadURI parses cloud string (example: us-1, us-2, eu-1, us-gov-1, etc) and returns a URI for uploading a container image for ImageAssessment.
@@ -41,25 +47,29 @@ func FalconContainerUploadURI(falconCloud CloudType) string {
 // When the Falcon Cloud CloudType is set to CloudAutoDiscover, be sure to provide the results of ApiConfig.Cloud after the client
 // has been initialized to ensure the correct CloudType is used and not CloudAutoDiscover.
 func FalconContainerSensorImageURI(falconCloud CloudType, sensorType SensorType) string {
+	return FalconImageURIWithCSRegistryEnv(falconCloud, sensorType, Release)
+}
+
+func FalconImageURIWithCSRegistryEnv(falconCloud CloudType, sensorType SensorType, csRegistryEnv CSRegistryEnv) string {
 	switch sensorType {
 	case SidecarSensor:
-		return fmt.Sprintf("%s/falcon-container/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-container/%s/%s/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case ImageSensor:
-		return fmt.Sprintf("%s/falcon-imageanalyzer/%s/release/falcon-imageanalyzer", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-imageanalyzer/%s/%s/falcon-imageanalyzer", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case KacSensor:
-		return fmt.Sprintf("%s/falcon-kac/%s/release/falcon-kac", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-kac/%s/%s/falcon-kac", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case NodeSensor:
-		return fmt.Sprintf("%s/falcon-sensor/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-sensor/%s/%s/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case Snapshot:
-		return fmt.Sprintf("%s/falcon-snapshot/%s/release/cs-snapshotscanner", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-snapshot/%s/%s/cs-snapshotscanner", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case FCSCli:
-		return fmt.Sprintf("%s/fcs/%s/release/cs-fcs", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/fcs/%s/%s/cs-fcs", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case SHRAController:
-		return fmt.Sprintf("%s/falcon-selfhostedregistryassessment/release/falcon-jobcontroller", registryFQDN(falconCloud))
+		return fmt.Sprintf("%s/falcon-selfhostedregistryassessment/%s/falcon-jobcontroller", registryFQDN(falconCloud), csRegistryEnv)
 	case SHRAExecutor:
-		return fmt.Sprintf("%s/falcon-selfhostedregistryassessment/release/falcon-registryassessmentexecutor", registryFQDN(falconCloud))
+		return fmt.Sprintf("%s/falcon-selfhostedregistryassessment/%s/falcon-registryassessmentexecutor", registryFQDN(falconCloud), csRegistryEnv)
 	default:
-		return fmt.Sprintf("%s/falcon-sensor/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-sensor/%s/%s/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	}
 }
 
