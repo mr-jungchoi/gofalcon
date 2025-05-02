8c4231fb5f747187cfea36bc224897d3cd4dde57
Jung Choi <jung.choi@crowdstrike.com>
2025-05-02 10:52:32 -0400

    add method to get Falcon image registry URI for testing

--- falcon/containers.go
+++ falcon/containers.go
@@ -5,12 +5,18 @@ import (
 )
 
 type SensorType string
+type CSRegistryEnv string
 
 const (
 	SidecarSensor SensorType = "falcon-container"
 	ImageSensor   SensorType = "falcon-imageanalyzer"
 	KacSensor     SensorType = "falcon-kac"
 	NodeSensor    SensorType = "falcon-sensor"
+)
+
+const (
+	Stage   CSRegistryEnv = "stage"
+	Release CSRegistryEnv = "release"
 )
 
 // FalconContainerUploadURI parses cloud string (example: us-1, us-2, eu-1, us-gov-1, etc) and returns a URI for uploading a container image for ImageAssessment.
@@ -31,17 +37,21 @@ func FalconContainerUploadURI(falconCloud CloudType) string {
 
 // FalconContainerSensorImageURI returns a URI for downloading a container sensor image. Defaults to the falcon-sensor image.
 func FalconContainerSensorImageURI(falconCloud CloudType, sensorType SensorType) string {
+	return FalconImageURIWithCSRegistryEnv(falconCloud, sensorType, Release)
+}
+
+func FalconImageURIWithCSRegistryEnv(falconCloud CloudType, sensorType SensorType, csRegistryEnv CSRegistryEnv) string {
 	switch sensorType {
 	case SidecarSensor:
-		return fmt.Sprintf("%s/falcon-container/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-container/%s/%s/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case ImageSensor:
-		return fmt.Sprintf("%s/falcon-imageanalyzer/%s/release/falcon-imageanalyzer", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-imageanalyzer/%s/%s/falcon-imageanalyzer", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case KacSensor:
-		return fmt.Sprintf("%s/falcon-kac/%s/release/falcon-kac", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-kac/%s/%s/falcon-kac", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	case NodeSensor:
-		return fmt.Sprintf("%s/falcon-sensor/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-sensor/%s/%s/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	default:
-		return fmt.Sprintf("%s/falcon-sensor/%s/release/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud))
+		return fmt.Sprintf("%s/falcon-sensor/%s/%s/falcon-sensor", registryFQDN(falconCloud), registryCloud(falconCloud), csRegistryEnv)
 	}
 }
 
